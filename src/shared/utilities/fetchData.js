8093013a150c1439cd53a6f83ac2ef23ff7e4489
Veraclins <icstps4love@gmail.com>
2018-09-22 11:15:06 +0100

    #160056181 User Login, Password Reset Hosting the Frontend on Heroku (#13)
    * login and password
    
    * feat(auth): user login, reset password and others
    
    - add user login feature
    - add user reset password feature
    - fix some css mix ups
    - add API proxy link for API calls in development
    - implement feedback from demo
    - move query-sting module to /src
    - add static.json to fix react router error on heroku
    
    [Delivers #160056181 #160056180]
    
    * remove folder for current task
    
    * add global history object

--- src/shared/utilities/fetchData.js
+++ src/shared/utilities/fetchData.js
@@ -16,8 +16,11 @@ const fetchData = async (payload) => {
   const {
     url, method, data, headers,
   } = payload;
+  const baseURL = process.env.NODE_ENV !== 'development'
+    ? 'https://authors-haven-staging.herokuapp.com/api/v1'
+    : 'http://localhost:4000/api/v1';
   const response = await axios({
-    baseURL: 'https://authors-haven-staging.herokuapp.com/api/v1',
+    baseURL,
     url,
     method: method || 'get',
     data,
